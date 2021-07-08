41f2ba30624aee4ca1b193e3e7ddf12c402a8acb
Ancazo <simon.ng.y.j@gmail.com>
2021-07-08 12:35:21 +0800

    created search button component

---
+++ src/Components/SearchButton/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+const SearchButton =() => {
+    return (
+  <input id='search' type="text" placeholder="Search" name="search"></input>
+    )
+}
+
+export default SearchButton
