b21c25638f2b9790b396512968c0993c3274a510
Yi ling <lingy93@gmail.com>
2021-07-10 14:19:18 +0800

    forgot to switch branch

---
+++ src/Pages/UploadPage/UploadPage.jsx
@@ -0,0 +1,33 @@
+
+import React from "react";
+import mainlogo from '../../Assets/mainlogo.png'
+import {
+    SplitPage,
+    FormPage,
+    Title,
+    ImageContainer,
+    PageContainer
+    } from "../../Components";
+import {
+    buttonStyling,
+    splitRight,
+    splitLeft,
+    } from './LandingPage.module.scss'
+
+export const LandingPage = () => {
+    return (
+        <PageContainer>
+            <SplitPage >
+                <div className = {`row ${splitLeft}`}>
+                    <ImageContainer src ={mainlogo} alt ='mainlogo'/>
+                    
+                </div>
+
+                <div className = {`col ${splitRight}`}>
+                    <Title title = 'Sign Up' comment='Sign up to get started!' />
+                    <FormPage buttonStyling = {buttonStyling}/>
+                </div>
+            </SplitPage>
+        </PageContainer>
+    )
+};
