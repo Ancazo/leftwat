88fad6e51e5cadcb62bda8d27d00d62153adc45d
Yi ling <lingy93@gmail.com>
2021-07-13 18:06:02 +0800

    table component and container done for upload section, form logic needs to be added

--- src/Pages/UploadPage/UploadPage.jsx
+++ src/Pages/UploadPage/UploadPage.jsx
@@ -1,30 +1,44 @@
 
-import React,{useEffect} from "react";
+import React,{useEffect, useState} from "react";
 import { ThemeToggleService } from "../../services";
 import {
     PageContainer,
     FileUploadButton,
-    OnePageContent
+    OnePageContent,
+    UploadTableContainer
     } from "../../Components";
 import {
     navStyling
     } from './UploadPage.module.scss'
+import { data } from '../../sample_data/data'
+
 
 export const UploadPage = () => {
 
     // set themeState
     ThemeToggleService('orange')
 
-
-    //upload image onto cloudinary here
+    // states for data to be transformed to table
+    const [dataArray,setDataArray] = useState([])
+    
     useEffect(() => {
+        //upload image onto cloudinary here
         console.log('uploadImage here')
     }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setDataArray(data)
+    }
     
     return (
         <PageContainer navlink ='/login' className = {navStyling}>
             <OnePageContent>
-                <FileUploadButton/>
+                <FileUploadButton submit={e=>handleSubmit(e)}/>
+                { dataArray.length !== 0 ?
+                    <UploadTableContainer data = {dataArray} />
+                    : ''
+                }
             </OnePageContent>
         </PageContainer>
     )
