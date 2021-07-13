b9a6e1a1e89aa3029966f7ee7e43fe640dfdfb84
Yi ling <lingy93@gmail.com>
2021-07-13 19:23:44 +0800

    remove react from themeprovider

--- src/services/ThemeToggleService.js
+++ src/services/ThemeToggleService.js
@@ -1,4 +1,4 @@
-import React,{useContext, useEffect} from "react";
+import {useContext, useEffect} from "react";
 import { ThemeContext } from './ThemeProvider'
 
 export const ThemeToggleService = (color) => {
